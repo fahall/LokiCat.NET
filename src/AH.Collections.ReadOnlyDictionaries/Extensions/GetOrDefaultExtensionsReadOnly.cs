4b13c1ca5102d776df5ab64b1f7659fffa0e7edf
Alex Hall <alex@alexhall.org>
2024-05-10 16:11:04 -0500

    Recreate projects from cmdline

---
+++ src/AH.Collections.ReadOnlyDictionaries/Extensions/GetOrDefaultExtensionsReadOnly.cs
@@ -0,0 +1,17 @@
+﻿namespace AH.Collections.ReadOnlyDictionaries.Extensions
+{
+    public static class GetOrDefaultExtensionsReadOnly
+    {
+        /// <summary>
+        /// Get value from readonly dictionary if it exists, otherwise return a default
+        /// </summary>
+        /// <param name="dict">The dictionary to search</param>
+        /// <param name="key">The key to lookup</param>
+        /// <param name="defaultValue">The value to return if the key does not exist</param>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <returns></returns>
+        public static TValue GetOrDefault<TKey, TValue>(this IReadOnlyDictionary<TKey, TValue> dict, TKey key, TValue defaultValue) =>
+            dict.ContainsKey(key) ? dict[key] : defaultValue;
+    }
+}
